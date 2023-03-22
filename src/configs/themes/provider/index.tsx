d26fb0505053800b6b8175c5fb6a279d92b3e9c3
vungbt1999 <bidinos1804@gmail.com>
2023-03-22 21:13:34 +0700

    feature/update-theme-base

--- src/configs/themes/provider/index.tsx
+++ src/configs/themes/provider/index.tsx
@@ -5,70 +5,27 @@ export const getStyle = (uiConfig: UIConfig) => {
   let configStyle: any = {
     // --- COLOR ---
     // Primary Color
-    '--color-primary': uiConfig.colors.primary.default,
-    '--color-primary-50': uiConfig.colors.primary._50,
-    '--color-primary-100': uiConfig.colors.primary._100,
-    '--color-primary-200': uiConfig.colors.primary._200,
     '--color-primary-300': uiConfig.colors.primary._300,
     '--color-primary-400': uiConfig.colors.primary._400,
     '--color-primary-500': uiConfig.colors.primary._500,
-    '--color-primary-600': uiConfig.colors.primary._600,
-    '--color-primary-700': uiConfig.colors.primary._700,
-    '--color-primary-900': uiConfig.colors.primary._900,
+
+    // Secondary Color
+    '--color-secondary-300': uiConfig.colors.secondary?._300,
+
+    // Light Color
+    '--color-light-300': uiConfig.colors.light?.default,
+
+    // Dark Color
+    '--color-dark-500': uiConfig.colors.dark?._500,
+    '--color-dark-600': uiConfig.colors.dark?._600,
 
     // Gray Color
-    '--color-gray': uiConfig.colors.gray?.default,
-    '--color-gray-50': uiConfig.colors.gray?._50,
-    '--color-gray-100': uiConfig.colors.gray?._100,
-    '--color-gray-200': uiConfig.colors.gray?._200,
-    '--color-gray-300': uiConfig.colors.gray?._300,
     '--color-gray-400': uiConfig.colors.gray?._400,
-    '--color-gray-500': uiConfig.colors.gray?._500,
-    '--color-gray-600': uiConfig.colors.gray?._600,
-    '--color-gray-700': uiConfig.colors.gray?._700,
-    '--color-gray-900': uiConfig.colors.gray?._900,
-
-    // Grey Color
-    '--color-grey': uiConfig.colors.grey?.default,
-
-    '--color-blue': uiConfig.colors.blue?.default,
-    '--color-indigo': uiConfig.colors.indigo?.default,
-    '--color-purple': uiConfig.colors.purple?.default,
-    '--color-pink': uiConfig.colors.pink?.default,
-    '--color-red': uiConfig.colors.red?.default,
-    '--color-orange': uiConfig.colors.orange?.default,
-    '--color-yellow': uiConfig.colors.yellow?.default,
-    '--color-green': uiConfig.colors.green?.default,
-    '--color-cyan': uiConfig.colors.cyan?.default,
-    '--color-teal': uiConfig.colors.teal?.default,
-    '--color-secondary': uiConfig.colors.secondary?.default,
-    '--color-success': uiConfig.colors.success?.default,
-    '--color-info': uiConfig.colors.info?.default,
-    '--color-warning': uiConfig.colors.warning?.default,
-    '--color-danger': uiConfig.colors.danger?.default,
-    '--color-light': uiConfig.colors.light?.default,
-    '--color-dark': uiConfig.colors.dark?.default,
-    '--color-facebook': uiConfig.colors.facebook?.default,
-    '--color-twitter': uiConfig.colors.twitter?.default,
-    '--color-linkedin': uiConfig.colors.linkedin?.default,
 
     // --- FONT ---
-    '--font-primary': uiConfig.font.primary
+    '--font-primary': uiConfig.font.primary,
+    '--font-secondary': uiConfig.font.secondary
   };
-
-  if (uiConfig.font.secondary) {
-    configStyle = {
-      ...configStyle,
-      '--font-secondary': uiConfig.font.secondary
-    };
-  }
-  if (uiConfig.font.tertiary) {
-    configStyle = {
-      ...configStyle,
-      '--font-tertiary': uiConfig.font.tertiary
-    };
-  }
-
   return configStyle;
 };
 
--- src/configs/themes/theme.ts
+++ src/configs/themes/theme.ts
@@ -2,93 +2,34 @@ import { UIConfig } from './types';
 
 export const themeConfig: UIConfig = {
   colors: {
-    blue: {
-      default: '#0d6efd'
-    },
-    indigo: {
-      default: '#6610f2'
-    },
-    purple: {
-      default: '#6f42c1'
-    },
-    pink: {
-      default: '#d63384'
-    },
-    red: {
-      default: '#dc3545'
-    },
-    orange: {
-      default: '#fd7e14'
-    },
-    yellow: {
-      default: '#ffc107'
-    },
-    green: {
-      default: '#198754'
-    },
-    cyan: {
-      default: '#0dcaf0'
-    },
-    teal: {
-      default: '#20c997'
-    },
     primary: {
-      default: '#ff6f61'
+      default: '#FFCC21',
+      _300: '#FFCC21',
+      _400: '#FF963C',
+      _500: '#EA6C00'
     },
     secondary: {
-      default: '#a6a6a6'
-    },
-    success: {
-      default: '#28bb74'
-    },
-    info: {
-      default: '#3b86ff'
-    },
-    warning: {
-      default: '#ff9736'
-    },
-    danger: {
-      default: '#ff6f61'
-    },
-    gray: {
-      default: '#3e3e3e',
-      _50: '#2b2b2b',
-      _100: '#e5e5e5',
-      _200: '#d8d8d8',
-      _300: '#bababa',
-      _400: '#909090',
-      _500: '#525252',
-      _600: '#3e3e3e',
-      _700: '#303030',
-      _800: '#2b2b2b',
-      _900: '#1f1f1f'
-    },
-    grey: {
-      default: '#767676'
+      default: '#8FE9D0',
+      _300: '#8FE9D0'
     },
     light: {
-      default: '#f5f5f5'
+      default: '#FFFFFF'
     },
     dark: {
-      default: '#1f1f1f'
+      default: '#2E2E2E',
+      _500: '#414141',
+      _600: '#2E2E2E'
     },
-    facebook: {
-      default: '#475a96'
-    },
-    twitter: {
-      default: '#49b2e8'
-    },
-    linkedin: {
-      default: '#0d76b4'
+    gray: {
+      default: '#777777',
+      _400: '#777777'
     }
   },
   font: {
-    primary: "'Jost', sans-serif",
-    secondary: "'Poppins', sans-serif",
-    tertiary: "'Open Sans', sans-serif"
+    primary: "'Noto Sans JP', sans-serif",
+    secondary: "'Inter', sans-serif"
   },
   stylesheets: [
-    'https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap',
-    'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
+    'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap'
   ]
 };
